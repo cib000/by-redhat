63dd70c031730acbf3d7e1a2666a87293120010d
by R.H <cib000@hanmail.net>
2026-08-17 22:08:57 +0900

    19

---
+++ docs/local_calenda/events.js
@@ -0,0 +1,18 @@
+﻿const scheduleData = {
+    "schedules": [
+    {
+      "id": "1",
+      "date": "2026-01-01",
+      "title": "이달을 정리해보기",
+      "type": "메모",
+      "repeat": "말일"
+    },
+    {
+      "id": "2",
+      "date": "2026-08-25",
+      "title": "공과 세금 정산일",
+      "type": "공과금",
+      "repeat": "매월"
+    }
+  ]
+};
---
+++ docs/local_calenda/title.js
@@ -0,0 +1,3 @@
+﻿const titleData = {
+  "calendarTitle": "🗓  ㅁ자 일정 달력  📆"
+};
