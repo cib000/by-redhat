4ca5e950db61558aba3d2b7cedf2cebca040be30
by R.H <cib000@hanmail.net>
2026-08-28 07:49:20 +0900

    Update events.js

--- docs/local_calenda/events.js
+++ docs/local_calenda/events.js
@@ -1,18 +1,25 @@
-﻿const scheduleData = {
+const scheduleData = {
     "schedules": [
-    {
-      "id": "1",
-      "date": "2026-01-01",
-      "title": "이달을 정리해보기",
-      "type": "메모",
-      "repeat": "말일"
-    },
-    {
-      "id": "2",
-      "date": "2026-08-25",
-      "title": "공과 세금 정산일",
-      "type": "공과금",
-      "repeat": "매월"
-    }
-  ]
+        {
+            "id": "1",
+            "date": "2026-01-01",
+            "title": "이달을 정리해보기",
+            "type": "메모",
+            "repeat": "없음"
+        },
+        {
+            "id": "2",
+            "date": "2026-08-25",
+            "title": "공과 세금 정산일",
+            "type": "공과금",
+            "repeat": "매월"
+        },
+        {
+            "id": "3",
+            "date": "2026-08-28",
+            "title": "전체 압축 다운로드 등록",
+            "type": "메모",
+            "repeat": "없음"
+        }
+    ]
 };
